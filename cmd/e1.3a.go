b6475cb48648216a02e7ca7923d59be30b05c314
Phil Bracikowski <13472206+philjb@users.noreply.github.com>
2020-07-08 15:12:35 -0700

    exercise 1.3 performance comp

---
+++ cmd/e1.3a.go
@@ -0,0 +1,15 @@
+// echo prints its command-line arguments
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	fmt.Println(strings.Join(os.Args[1:], " "))
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
